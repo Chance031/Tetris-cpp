bc46f10bdcdd1b130f63179f299ca21d4501150b
찬스 <aday031@naver.com>
2026-04-07 23:34:39 +0900

    feat(Test): 보드와 피스 테스트 2

--- Tetris/Tetris/main.cpp
+++ Tetris/Tetris/main.cpp
@@ -5,15 +5,27 @@
 int main()
 {
 	Board board;
-	Tetromino piece(TetrominoType::T);
+	Tetromino piece_1(TetrominoType::T);
 
-	piece.SetPosition(4, 0);
+	piece_1.SetPosition(4, 0);
 
-	if (board.CanPlace(piece))
+	if (board.CanPlace(piece_1))
 	{
-		board.LockPiece(piece);
+		board.LockPiece(piece_1);
 		std::cout << "Piece locked.\n";
 	}
+
+	Tetromino piece_2(TetrominoType::I);
+	
+	piece_2.SetPosition(4, 0);
+
+	if (board.CanPlace(piece_2))
+	{
+		board.LockPiece(piece_2);
+		std::cout << "Piece locked.\n";
+	}
+	else
+		std::cout << "이미 블록이 존재합니다.\n";
 
 	const int clearedLines = board.ClearLines();
 	std::cout << "Cleared Lines: " << clearedLines << '\n';
